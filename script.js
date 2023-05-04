9cf22003391a0d20d41d6b74d0abcfadf82a4d9b
dezoliveira <andresoliveira@protonmail.com>
2023-05-04 20:20:44 -0300

    render ajusts and prev and next buttons

--- script.js
+++ script.js
@@ -1,47 +1,65 @@
 const date = new Date()
 
-const monthDays = document.querySelector('.days')
+const renderCalendar = () => {
+  const monthDays = document.querySelector('.days')
 
-const lastDay = new Date(date.getFullYear(), date.getMonth() +1, 0).getDate()
+  const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate()
 
-const prevLastDay = new Date(date.getFullYear(), date.getMonth(), 0).getDate()
+  const prevLastDay = new Date(date.getFullYear(), date.getMonth(), 0).getDate()
 
-const firstDayIndex = date.getDay()
+  const firstDayIndex = date.getDay()
 
-const lastDayIndex = new Date(date.getFullYear(), date.getMonth() +1, 0).getDay()
+  const lastDayIndex = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDay()
 
-const nextDays = 7 - lastDayIndex
+  const nextDays = 7 - lastDayIndex - 1
 
-const months = [
-  "Janeiro",
-  "Fevereiro",
-  "Março",
-  "Abril",
-  "Maio",
-  "Junho",
-  "Julho",
-  "Agosto",
-  "Setembro",
-  "Outubro",
-  "Novembro",
-  "Dezembro",
-]
+  const months = [
+    "Janeiro",
+    "Fevereiro",
+    "Março",
+    "Abril",
+    "Maio",
+    "Junho",
+    "Julho",
+    "Agosto",
+    "Setembro",
+    "Outubro",
+    "Novembro",
+    "Dezembro",
+  ]
 
-document.querySelector(".date h1").innerHTML = months[date.getMonth()]
+  document.querySelector(".date h1").innerHTML = months[date.getMonth()]
 
-document.querySelector(".date p").innerHTML = date.toLocaleDateString()
+  document.querySelector(".date p").innerHTML = new Date().toLocaleDateString()
 
-let days = ""
+  let days = ""
 
-for(let x = firstDayIndex; x > 0; x--){
-  days += `<div class="prev-date">${prevLastDay - x + 1}</div>`
+  for(let x = firstDayIndex; x > 0; x--){
+    days += `<div class="prev-date">${prevLastDay - x + 1}</div>`
+  }
+
+  for(let i = 1; i <= lastDay; i++) {
+    if(i === new Date().getDate() && date.getMonth() === new Date().getMonth()){
+      days += `<div class="today">${i}</div>`
+    }else {
+      days += `<div>${i}</div>`
+    }
+  }
+
+  for(let j = 1; j <= nextDays; j++) {
+    days += `<div class="next-date">${j}</div>`
+    monthDays.innerHTML = days
+  }
 }
 
-for(let i = 1; i <= lastDay; i++) {
-  days += `<div>${i}</div>`
-}
+document.querySelector(".prev").addEventListener('click', () => {
+  date.setMonth(date.getMonth() - 1)
+  renderCalendar()
+})
 
-for(let j = 1; j <= nextDays; j++) {
-  days += `<div class="next-date">${j}</div>`
-  monthDays.innerHTML = days
-}
+document.querySelector(".next").addEventListener('click', () => {
+  date.setMonth(date.getMonth() + 1)
+  renderCalendar()
+})
+
+renderCalendar()

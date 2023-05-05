a79d82a2200913867a24b1945a6ed61dda67ffae
dezoliveira <andresoliveira@protonmail.com>
2023-05-05 18:30:42 -0300

    bugfix

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 const date = new Date()
 
 const renderCalendar = () => {
+  date.setDate(1);
+  
   const monthDays = document.querySelector('.days')
 
   const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate()
 
   const prevLastDay = new Date(date.getFullYear(), date.getMonth(), 0).getDate()
 
+  console.log(prevLastDay)
+
   const firstDayIndex = date.getDay()
 
   const lastDayIndex = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDay()
 
+
+  console.log(lastDayIndex)
+
   const nextDays = 7 - lastDayIndex - 1
 
+  console.log(nextDays)
   const months = [
     "Janeiro",
     "Fevereiro",
